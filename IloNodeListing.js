abaccd37a7a85efff213c231ccf3bb0bfc812d86
Desiree Ann Guzman <desireeann000@gmail.com>
2024-12-18 15:03:31 +0800

    Commit with IloNodeListing.js

---
+++ IloNodeListing.js
@@ -0,0 +1,45 @@
+class Node {
+    constructor(value) {
+      this.value = value;     
+      this.next = null;     
+     }
+  }
+  
+  class LinkedList {
+    constructor() {
+      this.head = null;       
+      this.size = 0;         
+    }
+  
+    append(value) {
+      const newNode = new Node(value);
+      if (this.head === null) {
+        this.head = newNode;    
+      } else {
+        let current = this.head;
+        while (current.next !== null) {
+          current = current.next; 
+        }
+        current.next = newNode;   
+      }
+      this.size++;
+      this.print();           
+    }
+  
+    print() {
+      let current = this.head;
+      let result = [];
+      while (current !== null) {
+        result.push(current.value);
+        current = current.next;
+      }
+      console.log(`Linked List: ${result.join(" -> ")}`);
+    }
+  }
+  
+  const linkedList = new LinkedList();
+  
+  linkedList.append("Data Structures – Array");
+  linkedList.append("Variable Type – Integer");
+  linkedList.append("Sorting Algorithm – Bubble Sort");
+  
